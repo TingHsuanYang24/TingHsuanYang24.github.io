1fc7354f47f1c1740456a6fcb87d268b8f79ad51
Yang <t_y24@txstate.edu>
2019-09-23 19:29:55 -0500

    Completed Task 5. Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,4 @@
+/*
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
 
@@ -6,3 +7,19 @@ const y = 33;
 document.writeln(x + y);
 document.writeln("<br>");
 document.writeln(x += 33);
+*/
+
+document.writeln("<button onclick='condition()'>Conditional Test</button>")
+
+function condition()
+{
+  x = confirm("are you sure you want to proceed?");
+  if(x)
+  {
+    document.writeln("You chose Okay!");
+  }
+  else
+  {
+  document.writeln("You choose Cancel!");
+  }
+}

e3e62c1445e197eb209df3122a31f0f2718a8689
Yang <t_y24@txstate.edu>
2019-09-23 19:17:30 -0500

    Completed Task 3. Variables

--- javascript/external.js
+++ javascript/external.js
@@ -1,4 +1,8 @@
 //window.alert(5 + 6);
 //window.alert('5+6');
-document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>');
+//document.write('<h1>A heading</h1>');
+//document.write('<p>A sentence.</p>');
+
+// A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
